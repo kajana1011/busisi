6377781f6facafc1a6713cc066e956d55e5b77e7
revocajana <revocajana@gmail.com>
2025-11-20 15:31:02 +0300

    removing default mysql credentials

--- config/database.php
+++ config/database.php
@@ -6,8 +6,8 @@
 // Database connection settings
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'busisi');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'busisiadmin');
+define('DB_PASS', 'admin123');
 define('DB_CHARSET', 'utf8mb4');
 
 // Display errors (set to false in production)
